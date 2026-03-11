22199809d6e920cd4b13d16d707fa9a253a5cea0
Nicolas A. Romero <nicolas.romeroc@hotmail.com>
2026-03-11 11:32:19 -0500

    style: Añadido el link en el inicio para cuando se comparta el cv como pdf y el harea de trayectoria.

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 import './src/style.css'
 
 console.log('Jeefry Nicolas Archila Romero - Resume');
+
+// Typewriter Effect
+const typewriterElement = document.getElementById('typewriter');
+const textToType = "https://coralgamer.github.io/hojadevida/";
+let index = 0;
+let isTyping = false;
+
+function type() {
+  if (index < textToType.length) {
+    typewriterElement.textContent += textToType.charAt(index);
+    index++;
+    setTimeout(type, 100);
+  }
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && !isTyping) {
+      isTyping = true;
+      type();
+    }
+  });
+}, { threshold: 0.5 });
+
+if (typewriterElement) {
+  observer.observe(typewriterElement);
+}
